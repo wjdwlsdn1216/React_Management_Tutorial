1c380e5efa6fd537be3531235eea2ab279639103
Jinwoo Jung <wjdwlsdn1216@naver.com>
2020-12-27 02:57:24 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '정진우',
+  'birthday' : '931216',
+  'gender' : '남자',
+  'job' : '취준생'
+}
+
+class App extends Component {
+  render() {
+    return(
+      <Customer
+        id={customer.id}
+        image={customer.image}
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
