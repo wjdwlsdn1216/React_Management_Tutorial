2c8470abf2e01c85fb8ada1c7645e92d4fa5df26
Jinwoo Jung <wjdwlsdn1216@naver.com>
2020-12-28 10:42:38 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer'
 
-const customer = {
+const customers = [
+  {
   'id' : 1,
-  'image' : 'https://placeimg.com/64/64/any',
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : '정진우',
   'birthday' : '931216',
   'gender' : '남자',
   'job' : '취준생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '931216',
+  'gender' : '남자',
+  'job' : '프로그래머'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '나길동',
+  'birthday' : '931216',
+  'gender' : '남자',
+  'job' : '의사'
 }
+]
 
 class App extends Component {
   render() {
     return(
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
     );
   }
 }
