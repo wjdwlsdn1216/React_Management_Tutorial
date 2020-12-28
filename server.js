2c756a47a14431e90fc1731579d617cc9375917b
Jinwoo Jung <wjdwlsdn1216@naver.com>
2020-12-28 13:43:05 +0900

    Add Get Customers Rest Api Module

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '정진우',
+            'birthday': '931216',
+            'gender': '남자',
+            'job': '취준생'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '홍길동',
+            'birthday': '931216',
+            'gender': '남자',
+            'job': '프로그래머'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '나길동',
+            'birthday': '931216',
+            'gender': '남자',
+            'job': '의사'
+        }
+    ]);
 });
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
